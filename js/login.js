39b8844a3a782293dd9029263185d384b8c5f7b4
Isaac de la Cruz <isaac.delacruz@iteso.mx>
2023-12-03 22:53:59 -0600

    Added missing modals to create quizzes and questions

--- js/login.js
+++ js/login.js
@@ -1,16 +1,34 @@
 const loginForm = $('#loginForm')
-console.log(loginForm)
 loginForm.on('submit', e => {
     e.preventDefault();
     const emailInput = $('#emailInput')
     const passwordInput = $('#passwordInput')
 
+    const userData = {
+        email: emailInput.val(),
+        password: passwordInput.val()
+    };
+
+    console.log(JSON.stringify(userData), "user Data")
+
+
     console.log(emailInput.val(), "email")
     console.log(passwordInput.val(), "password")
-    jQuery.post( "https://quizmaniabackend.onrender.com/login",
-        {email: emailInput.val(), password: passwordInput.val()},
-        () => {
-            console.log()
+    $.ajax({
+        url: 'https://quizmaniabackend.onrender.com/login',
+        type: 'post',
+        contentType: 'application/json',
+        data: JSON.stringify(userData),
+        processData: false,
+        success: res => {
+            console.log(res, "resss")
+            if (res) {
+                localStorage.setItem("token", res);
+                window.location.href = "/main"
+            }
+        else
+            alert("No token received")
         },
-        "json").done()
+        error: () => {alert("Correo o contraseña son incorrectos")}
+    })
 })
